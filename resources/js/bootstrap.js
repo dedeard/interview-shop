7cf5a7ff39335622af1233d3144dbcd0134ce5f9
Dede ariansya <me@dedeard.my.id>
2023-03-10 12:38:52 +0800

    Setup admin-lte

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,15 +1,12 @@
-import 'bootstrap';
+import "bootstrap";
+import "admin-lte";
+import axios from "axios";
+import Swal from "sweetalert2/dist/sweetalert2";
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-import axios from 'axios';
 window.axios = axios;
+window.Swal = Swal;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
